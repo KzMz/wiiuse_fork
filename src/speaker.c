f21bf9ed2738ee0c52d1c3456cd8326ccd1d40ca
Mattia Bruno <mrbrown2@tele2.it>
2010-01-01 19:02:00 +0100

    reuploaded

---
+++ src/speaker.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#include "definitions.h"
+#include "wiiuse_internal.h"
+
+
+void wiiuse_mute_speaker(struct wiimote_t *wm, int status) {
+
+}
+
+void wiiuse_set_speaker(struct wiimote_t *wm,int status)
+{
+
+}
+
+void wiiuse_play_sound(struct wiimote_t* wm, byte* data, int size, byte freq) {
+
+}
+
